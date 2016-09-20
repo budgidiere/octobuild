99e5fb2bbda599e17b393f021589d800e1ea6b5b
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-09-20 13:24:54 +0300

    Test compilation fix

--- src/vs/postprocess.rs
+++ src/vs/postprocess.rs
@@ -560,7 +560,7 @@ mod test {
         for source in sources.iter() {
             wrapper.sources.insert(source.as_bytes());
         }
-        match super::filter_preprocessed(&mut Cursor::new(stream), &mut wrapper, marker, keep_headers) {
+        match super::filter_preprocessed(&mut Cursor::new(stream), &mut wrapper, true, marker, keep_headers) {
             Ok(_) => {
                 let actual = String::from_utf8_lossy(&wrapper.content);
                 if actual != expected.replace("\n", eol) {
