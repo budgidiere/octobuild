4d1c2df1e7bebd37fad2aa790e5df541f9addc86
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-09-21 12:00:38 +0300

    Fix preprocess_batch configuration parameter for octo_cl tool

--- src/bin/octo_cl.rs
+++ src/bin/octo_cl.rs
@@ -1,9 +1,14 @@
 extern crate octobuild;
+extern crate tempdir;
+
+use tempdir::TempDir;
 
 use octobuild::vs::compiler::VsCompiler;
 use octobuild::simple::simple_compile;
+
 use std::process;
+use std::sync::Arc;
 
 fn main() {
-    process::exit(simple_compile("cl.exe", |_| VsCompiler::default()))
+    process::exit(simple_compile("cl.exe", |config| Ok(VsCompiler::new(&Arc::new(try!(TempDir::new("octobuild"))), config.preprocess_batch))))
 }
