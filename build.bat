e3298d11201a51eb1d94b595f8b99b79ee0b70c5
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-09-26 16:11:07 +0300

    Update build.bat for more simple testing

--- build.bat
+++ build.bat
@@ -1,2 +1,6 @@
 cd %~dp0
-cargo build --release --target i686-pc-windows-gnu && cargo build --release --target x86_64-pc-windows-gnu && %WIXSHARP_DIR%\cscs.exe wixcs\setup.cs
+cargo build --release --target i686-pc-windows-gnu || exit 1
+cargo build --release --target x86_64-pc-windows-gnu || exit 1
+copy /Y target\i686-pc-windows-gnu\release\octobuild.dll target\octobuild.x86.dll
+copy /Y target\x86_64-pc-windows-gnu\release\octobuild.dll target\octobuild.x64.dll
+%WIXSHARP_DIR%\cscs.exe wixcs\setup.cs || exit 1
