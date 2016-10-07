dab7a09969d5db26263d71b6a4f4b5ea785c9a47
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-10-07 16:53:08 +0300

    Implement Visual Studio #include behaviour

--- src/bin/test_includes.rs
+++ src/bin/test_includes.rs
@@ -1,7 +1,6 @@
 extern crate octobuild;
 
-use octobuild::filter::includes::source_includes;
-use octobuild::direct::scanner::collect_includes;
+use octobuild::direct::scanner::{IncludeBehaviour, collect_includes};
 
 use std::env;
 use std::fs::File;
@@ -11,22 +10,18 @@ use std::path::Path;
 fn main() {
     for arg in env::args().skip(1) {
         println!("File: {}", arg);
-        let buffer = File::open(Path::new(&arg))
-            .and_then(|mut f| {
-                let mut v = Vec::new();
-                f.read_to_end(&mut v).map(|_| v)
-            })
-            .unwrap();
-        //let res = source_includes(&buffer);
-        let res = collect_includes(Path::new(&arg), &[
-            Path::new("/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../../include/c++/5.4.0"),
-            Path::new("/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../../include/x86_64-linux-gnu/c++/5.4.0"),
-            Path::new("/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../../include/c++/5.4.0/backward"),
-            Path::new("/usr/local/include"),
-            Path::new("/usr/lib/llvm-3.8/bin/../lib/clang/3.8.0/include"),
-            Path::new("/usr/include/x86_64-linux-gnu"),
-            Path::new("/usr/include"),
-        ]);
+        let res = collect_includes(Path::new(&arg),
+                                   &[Path::new("/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../..\
+                                                /include/c++/5.4.0"),
+                                     Path::new("/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../..\
+                                                /include/x86_64-linux-gnu/c++/5.4.0"),
+                                     Path::new("/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../..\
+                                                /include/c++/5.4.0/backward"),
+                                     Path::new("/usr/local/include"),
+                                     Path::new("/usr/lib/llvm-3.8/bin/../lib/clang/3.8.0/include"),
+                                     Path::new("/usr/include/x86_64-linux-gnu"),
+                                     Path::new("/usr/include")],
+                                   &IncludeBehaviour::VisualStudio);
         println!("{:?}", res);
     }
 }
--- src/direct/scanner.rs
+++ src/direct/scanner.rs
@@ -1,8 +1,9 @@
 use std::collections::HashSet;
-use std::path::{PathBuf, Path};
-use std::io::{Error, Read};
 use std::fs::File;
+use std::io::{Error, Read};
+use std::iter;
 use std::rc::Rc;
+use std::path::{Path, PathBuf};
 
 use ::filter::includes::{Include, source_includes};
 
@@ -15,29 +16,68 @@ struct CollectTask {
     source: PathBuf,
 }
 
-pub fn collect_includes(source_file: &Path, include_dir: &[&Path]) -> Result<HashSet<PathBuf>, Error> {
+pub trait IncludeCombine {
+    fn combine_context(&self, context: &Rc<Vec<PathBuf>>, source_path: &Path) -> Rc<Vec<PathBuf>>;
+}
+
+pub enum IncludeBehaviour {
+    Clang,
+    VisualStudio,
+}
+
+pub fn collect_includes(source_file: &Path,
+                        include_dir: &[&Path],
+                        combine: &IncludeCombine)
+                        -> Result<HashSet<PathBuf>, Error> {
     let mut queue: Vec<CollectTask> = Vec::new();
     let mut result: HashSet<CollectTask> = HashSet::new();
 
     let source_canon = try!(source_file.canonicalize());
-    queue.push(CollectTask { context: combine_context(&Rc::new(Vec::new()), &source_canon), source: source_canon });
+    queue.push(CollectTask {
+        context: combine.combine_context(&Rc::new(Vec::new()), &source_canon),
+        source: source_canon,
+    });
     loop {
         match queue.pop() {
             Some(task) => {
                 if result.insert(task.clone()) {
-                    for include in try!(file_include_paths(&task.source, &[task.source.parent().unwrap()], include_dir)).into_iter() {
-                        queue.push(CollectTask { context: combine_context(&task.context, &include), source: include });
+                    for include in try!(file_include_paths(&task.source, &task.context[..], include_dir)).into_iter() {
+                        queue.push(CollectTask {
+                            context: combine.combine_context(&task.context, &include),
+                            source: include,
+                        });
                     }
                 }
-            },
-            None => { break; }
+            }
+            None => {
+                break;
+            }
         }
     }
     Ok(result.into_iter().map(|t| t.source).collect())
 }
 
-fn combine_context(context: &Rc<Vec<PathBuf>>, source_path: &Path) -> Rc<Vec<PathBuf>> {
-    Rc::new(source_path.parent().map(|p| p.to_path_buf()).into_iter().collect())
+impl IncludeCombine for IncludeBehaviour {
+    fn combine_context(&self, context: &Rc<Vec<PathBuf>>, source_path: &Path) -> Rc<Vec<PathBuf>> {
+        match self {
+            &IncludeBehaviour::Clang => Rc::new(source_path.parent().map(|p| p.to_path_buf()).into_iter().collect()),
+            &IncludeBehaviour::VisualStudio => {
+                match source_path.parent() {
+                    Some(source_dir) => {
+                        if context.last().map_or(false, |v| v.as_path() == source_dir) {
+                            return context.clone();
+                        }
+                        Rc::new(context.iter()
+                            .filter(|v| v.as_path() != source_dir)
+                            .map(|v| v.clone())
+                            .chain(iter::once(source_dir.to_path_buf()))
+                            .collect())
+                    }
+                    None => context.clone(),
+                }
+            }
+        }
+    }
 }
 
 fn file_includes(source_file: &Path) -> Result<Vec<Include<String>>, Error> {
@@ -49,20 +89,35 @@ fn file_includes(source_file: &Path) -> Result<Vec<Include<String>>, Error> {
         .and_then(|b| source_includes(&b))
 }
 
-fn file_include_paths(source_file: &Path, context_dir: &[&Path], include_dir: &[&Path]) -> Result<Vec<PathBuf>, Error> {
-    file_includes(source_file)
-        .and_then(|v| v.into_iter().filter_map(|i|
-            match i {
-                Include::Quote(path) => solve_include_path(Path::new(&path), context_dir).or_else(|| solve_include_path(Path::new(&path), include_dir)),
-                Include::Bracket(path) => solve_include_path(Path::new(&path), include_dir),
-            }).collect())
+fn file_include_paths(source_file: &Path,
+                      context_dir: &[PathBuf],
+                      include_dir: &[&Path])
+                      -> Result<Vec<PathBuf>, Error> {
+    println!("> {:?} ({:?})", source_file, context_dir);
+    file_includes(source_file).and_then(|v| {
+        v.into_iter()
+            .filter_map(|i| match i {
+                Include::Quote(name) => {
+                    let path = Path::new(&name);
+                    solve_include_path(path,
+                                       context_dir.iter()
+                                           .rev()
+                                           .map(|p| p.as_path())
+                                           .chain(include_dir.iter().map(|p| *p)))
+                }
+                Include::Bracket(name) => solve_include_path(Path::new(&name), include_dir.iter().map(|p| *p)),
+            })
+            .collect()
+    })
 }
 
-fn solve_include_path(include_path: &Path, search_dir: &[&Path]) -> Option<Result<PathBuf, Error>> {
+fn solve_include_path<'a, I: Iterator<Item = &'a Path>>(include_path: &Path,
+                                                        dir_iter: I)
+                                                        -> Option<Result<PathBuf, Error>> {
     if include_path.is_absolute() {
         return Some(Ok(include_path.to_path_buf()));
     }
-    for dir in search_dir.iter() {
+    for dir in dir_iter {
         let path = dir.join(include_path);
         if path.is_file() {
             return Some(path.canonicalize());
