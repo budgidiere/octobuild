80f0747923a2dee0173592ef1664dbdc66f45d14
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-12-08 07:40:22 +0300

    Add VS2017 execution sample

---
+++ samples/vs2017/make.bat
@@ -0,0 +1,2 @@
+@call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+nmake clean all
---
+++ samples/vs2017/sample.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#pragma once
+
+class Foo {
+};
